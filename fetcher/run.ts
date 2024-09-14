e9a0a093e57933016625808208788a3cd707a403
Ashley Chen <86508619+ashleyxchen@users.noreply.github.com>
2024-09-14 16:57:42 -0400

    init commit

--- fetcher/run.ts
+++ fetcher/run.ts
@@ -3,11 +3,7 @@ import os from 'os';
 import fs from 'fs';
 import path from 'path';
 
-const drive = google.drive('v3');
-
-async function runSample(fileId: string) {
-  // Obtain user credentials to use for the request
-  const auth = new google.auth.GoogleAuth({
+const auth = new google.auth.GoogleAuth({
     keyFile: path.join(__dirname, '../creds.json'),
     scopes: [
       'https://www.googleapis.com/auth/drive',
@@ -21,15 +17,28 @@ async function runSample(fileId: string) {
   });
   google.options({auth});
 
+const drive = google.drive('v3');
+const sheets = google.sheets('v4')
 
+
+async function downloadFile(fileId: string, email: string) {
   return drive.files
     .get({fileId, alt: 'media'}, {responseType: 'stream'})
     .then(res => {
       return new Promise((resolve, reject) => {
-        const filePath = path.join(os.tmpdir(), fileId + ".jpg");
-        const dest = fs.createWriteStream(filePath);
-        console.log('writing to', filePath);
 
+        let type = ""
+        if (res.headers["content-type"] === "image/jpeg") {
+          type = ".jpg"
+        } else if (res.headers["content-type"] === "image/png") {
+          type = ".png"
+        } else {
+          console.log(`Unknown type ${res.headers["content-type"]}`)
+          return;
+        }
+
+        const filePath = path.join("photos", email + type);
+        const dest = fs.createWriteStream(filePath);
         res.data
           .on('end', () => {
             console.log('Done downloading file to',filePath);
@@ -39,5 +48,31 @@ async function runSample(fileId: string) {
     });
 }
 
-console.log('starting')
-runSample("1xM_ZwSokKCtZq1FFTAtIijydRYy7MQCH")
+async function loadImages() {
+    console.log("fetching....")
+    let downloaded = 0;
+    const res = await sheets.spreadsheets.values.get(
+      {
+        auth: auth,
+        spreadsheetId: '1ihI0L2EgxmN0_8GrVlKnBoKrikL3BOGryokf9j4V8n0',
+        range: 'Form Responses 1!A2:E',
+      }
+    );
+    const rows = res.data.values ?? [];
+    for (const row of rows) {
+        const email = row[1];
+        const imageURL = row[4];
+        const imgPath = path.join("photos", email);
+        if (!fs.existsSync(imgPath + ".jpg") && !fs.existsSync(imgPath + ".png")) {
+            await downloadFile(imageURL.replace("https://drive.google.com/open?id=", ""), email)
+            downloaded++;
+        } else {
+            console.log(`skipping ${email}`);
+        }
+    }
+    console.log(`Downloaded ${downloaded} images`);
+    setTimeout(loadImages, 5 * 1000);
+  }
+
+  loadImages()
+
