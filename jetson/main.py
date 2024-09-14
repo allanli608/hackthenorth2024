2a72ebf107588657c7c7c4944e272398d6ce85e5
Lavan Sumanan <lavan.sumanan@gmail.com>
2024-09-14 13:45:40 -0400

    initial servo + camera code

---
+++ jetson/main.py
@@ -0,0 +1,43 @@
+import Jetson.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+led_pin = 23
+servo_pin = 32
+
+GPIO.setup(servo_pin, GPIO.OUT)
+
+p = GPIO.PWM(servo_pin, 50)  # 50 Hz frequency
+# 2.5% duty cycle corresponds to 0 degrees
+# 12.5% duty cycle corresponds to 90 degrees
+# 22.5% duty cycle corresponds to 180 degrees
+p.start(2.5)  # Start PWM with 2.5% duty cycle (0 degrees)
+
+def set_angle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(32, True)
+    p.ChangeDutyCycle(duty)
+    time.sleep(round(angle/360, 2)) # delay so servo has time to move; proportional to angle
+    GPIO.output(32, False)
+    p.ChangeDutyCycle(0)
+
+try:
+    while True:
+        # Turn LED on
+        GPIO.output(led_pin, GPIO.HIGH)
+        # Rotate to 90 degrees
+        set_angle(90)
+        print("on")
+        time.sleep(1)
+        
+        # Turn LED off
+        GPIO.output(led_pin, GPIO.LOW)
+        # Rotate to 0 degrees
+        set_angle(0)
+        print("off")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    p.stop() # Stop PWM output
+    print("GPIO cleaned up")
