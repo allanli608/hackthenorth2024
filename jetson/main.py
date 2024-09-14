48603ed3f38d6a07f1b30efe33c24dc556f25b89
Lavan Sumanan <lavan.sumanan@gmail.com>
2024-09-14 19:53:42 -0400

    test with simple angles

--- jetson/main.py
+++ jetson/main.py
@@ -5,39 +5,42 @@ GPIO.setmode(GPIO.BOARD)
 led_pin = 23
 servo_pin = 32
 
+GPIO.setup(led_pin, GPIO.OUT)
 GPIO.setup(servo_pin, GPIO.OUT)
 
 p = GPIO.PWM(servo_pin, 50)  # 50 Hz frequency
 # 2.5% duty cycle corresponds to 0 degrees
 # 12.5% duty cycle corresponds to 90 degrees
 # 22.5% duty cycle corresponds to 180 degrees
-p.start(2.5)  # Start PWM with 2.5% duty cycle (0 degrees)
+p.start(0)
 
 def set_angle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(32, True)
+    duty = angle / 18. + 2.5
     p.ChangeDutyCycle(duty)
-    time.sleep(round(angle/360, 2)) # delay so servo has time to move; proportional to angle
-    GPIO.output(32, False)
-    p.ChangeDutyCycle(0)
+    time.sleep(1) # delay so servo has time to move; proportional to angle
+    # p.ChangeDutyCycle(0)
 
 try:
-    while True:
-        # Turn LED on
-        GPIO.output(led_pin, GPIO.HIGH)
-        # Rotate to 90 degrees
-        set_angle(90)
-        print("on")
-        time.sleep(1)
-        
-        # Turn LED off
-        GPIO.output(led_pin, GPIO.LOW)
-        # Rotate to 0 degrees
-        set_angle(0)
-        print("off")
-        time.sleep(1)
+    # while True:
+    # Turn LED on
+    GPIO.output(led_pin, GPIO.HIGH)
+    # Rotate to 90 degrees
+    set_angle(90)
+    print("on")
+    time.sleep(2)
+    
+    # Turn LED off
+    GPIO.output(led_pin, GPIO.LOW)
+    # Rotate to 0 degrees
+    set_angle(0)
+    print("off")
+    time.sleep(2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
     p.stop() # Stop PWM output
     print("GPIO cleaned up")
+
+finally:
+    GPIO.cleanup()
+    p.stop()
