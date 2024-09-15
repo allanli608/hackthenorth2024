14bdb461a47dc199541820d93a0283dac35d3e08
Lavan Sumanan <lavan.sumanan@gmail.com>
2024-09-14 21:48:20 -0400

    add serial communication to trigger servo attack

--- jetson/main.py
+++ jetson/main.py
@@ -1,46 +1,27 @@
-import Jetson.GPIO as GPIO
+import serial
 import time
 
-GPIO.setmode(GPIO.BOARD)
-led_pin = 23
-servo_pin = 32
+# Open the serial connection (adjust the port as needed)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.setup(servo_pin, GPIO.OUT)
+# Allow some time for the connection to establish
+time.sleep(2)
 
-p = GPIO.PWM(servo_pin, 50)  # 50 Hz frequency
-# 2.5% duty cycle corresponds to 0 degrees
-# 12.5% duty cycle corresponds to 90 degrees
-# 22.5% duty cycle corresponds to 180 degrees
-p.start(0)
+print('connected:', ser)
 
-def set_angle(angle):
-    duty = angle / 18. + 2.5
-    p.ChangeDutyCycle(duty)
-    time.sleep(1) # delay so servo has time to move; proportional to angle
-    # p.ChangeDutyCycle(0)
+def sendAttackSignal():
+	# Send a message to the Arduino
+	ser.write(b'1')
+	time.sleep(5)
+	ser.write(b'0')
 
-try:
-    # while True:
-    # Turn LED on
-    GPIO.output(led_pin, GPIO.HIGH)
-    # Rotate to 90 degrees
-    set_angle(90)
-    print("on")
-    time.sleep(2)
-    
-    # Turn LED off
-    GPIO.output(led_pin, GPIO.LOW)
-    # Rotate to 0 degrees
-    set_angle(0)
-    print("off")
-    time.sleep(2)
+# Main loop
+while True:
+	notVerified = True
+	if notVerified:
+		print("triggered attack!")
+		sendAttackSignal()
+	else:
+		print("no attack")
+	time.sleep(5)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    p.stop() # Stop PWM output
-    print("GPIO cleaned up")
-
-finally:
-    GPIO.cleanup()
-    p.stop()
